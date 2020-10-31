d50462e4f6a3c13dc96b7616c20a5d7a51719fc7
Rohit <rkumar8092378845@gmail.com>
2020-11-01 01:51:24 +0530

    -> animation - 5
    key Attributes

---
+++ app/src/main/java/com/rohitthebest/motionlayout/AnimationFive.kt
@@ -0,0 +1,11 @@
+package com.rohitthebest.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimationFive : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation_five)
+    }
+}
